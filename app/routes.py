c3351d0826bdaee750d67f1c9146d542616d4d26
ECano22 <156382454+ECano22@users.noreply.github.com>
2025-12-30 00:17:38 -0800

    Started work on Project.HTML
    a dynamic webpage which relies on python variables to display each project's content

--- app/routes.py
+++ app/routes.py
@@ -1,43 +1,52 @@
 from flask import render_template
 from app import app
 
+PROJECTS = {
+    "Console-Tile-Generator": {
+        "title": "Console Tile Generator",
+        "languages": ["C++", "test"],
+        "desc": "",
+        "githubLink": "https://github.com/ECano22/Portfolio/tree/main/C%2B%2B/WorldGen",
+        "shortdesc": "A procedural terrain generation engine that renders an infinite, explorable 2D world directly in the console.",
+        "pagelink": "",
+        "thumbnaillink": "ConsoleCPP.png",
+    },
+    "Data-Driven-Command-Line-RPG": {
+        "title": "Data-Driven Command-Line RPG",
+        "languages": ["Python"],
+        "desc": "",
+        "githubLink": "https://github.com/ECano22/Portfolio/tree/main/Python/Turn-Based%20Game",
+        "shortdesc": "A feature-complete, text-based RPG that simulates turn-based combat.",
+        "pagelink": "",
+        "thumbnaillink": "",
+    },
+    "Portfolio-Website": {
+        "title": "Portfolio Website",
+        "languages": ["Python", "HTML", "CSS", "JavaScript"],
+        "desc": "",
+        "githubLink": "https://github.com/ECano22/Portfolio-Website",
+        "shortdesc": "A website to host my portfolio projects.",
+        "pagelink": "",
+        "thumbnaillink": "",
+    },
+}
+
 @app.route('/')
 @app.route('/homepage')
 def homepage():
     return render_template('homepage.html')
 @app.route('/projects')
 def projects():
-    projects = [
-        #This should eventually be moved into JSON file
-        {
-            "id": 0,
-            "languages": ["C++"],
-            "title": "Console Tile Generator",
-            "desc": "A procedural terrain generation engine that renders an infinite, explorable 2D world directly in the console.",
-            "infolink": None,
-            "demolink": None,
-            "sourcelink": "https://github.com/ECano22/Portfolio/tree/main/C%2B%2B/WorldGen"
-        },
-        {
-            "id": 1,
-            "languages": ["Python"],
-            "title": "Data-Driven Command-Line RPG",
-            "desc": "A feature-complete, text-based RPG that simulates turn-based combat.",
-            "infolink": None,
-            "demolink": None,
-            "sourcelink": "https://github.com/ECano22/Portfolio/tree/main/Python/Turn-Based%20Game"
-        },
-        {
-            "id": 2,
-            "languages": ["Python", "HTML", "CSS", "JavaScript"],
-            "title": "Portfolio Website",
-            "desc": "A website to host my portfolio projects.",
-            "infolink": None,
-            "demolink": None,
-            "sourcelink": "https://github.com/ECano22/Portfolio-Website"
-        },
-    ]
-    return render_template('projects.html', projects=projects)
+    return render_template('projects.html', projects=PROJECTS)
+@app.route('/projects/<project_id>')
+def project_page(project_id):
+    try:
+        project = PROJECTS[project_id]
+        return render_template('project.html', project=project)
+    except KeyError:
+        #return 404 page, not created yet
+        return 
+
 @app.route('/aboutme')
 def aboutme():
     return render_template('aboutme.html')
