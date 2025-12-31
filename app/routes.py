7b39d5590059997aa3325b7da26355baf638838b
ECano22 <156382454+ECano22@users.noreply.github.com>
2025-12-31 01:13:59 -0800

    Finished work on Project.html
    Finished the html and css for project.html, all that is needed is .txt files for Portfolio Website

--- app/routes.py
+++ app/routes.py
@@ -1,35 +1,33 @@
-from flask import render_template
+from flask import render_template, current_app
 from app import app
+from pathlib import Path
 
 PROJECTS = {
     "Console-Tile-Generator": {
         "title": "Console Tile Generator",
-        "languages": ["C++", "test"],
-        "desc": "",
+        "imglink": "ConsoleCPPImage.png",
+        "languages": ["C++"],
+        "desctxt": "tilegenerator",
         "githubLink": "https://github.com/ECano22/Portfolio/tree/main/C%2B%2B/WorldGen",
         "shortdesc": "A procedural terrain generation engine that renders an infinite, explorable 2D world directly in the console.",
         "pagelink": "",
-        "thumbnaillink": "ConsoleCPP.png",
-    },
-    "Data-Driven-Command-Line-RPG": {
-        "title": "Data-Driven Command-Line RPG",
-        "languages": ["Python"],
-        "desc": "",
-        "githubLink": "https://github.com/ECano22/Portfolio/tree/main/Python/Turn-Based%20Game",
-        "shortdesc": "A feature-complete, text-based RPG that simulates turn-based combat.",
-        "pagelink": "",
-        "thumbnaillink": "",
+        "thumbnaillink": "ConsoleCPPThumbnail.png",
     },
     "Portfolio-Website": {
         "title": "Portfolio Website",
+        "imglink": "",
         "languages": ["Python", "HTML", "CSS", "JavaScript"],
-        "desc": "",
+        "desctxt": "portfolio",
         "githubLink": "https://github.com/ECano22/Portfolio-Website",
         "shortdesc": "A website to host my portfolio projects.",
         "pagelink": "",
-        "thumbnaillink": "",
+        "thumbnaillink": "portfoliowebsitethumbnail.png",
     },
 }
+
+#project description is kept in txt file
+BASE_DIR = Path(__file__).resolve().parent
+INFO_DIR = BASE_DIR / 'static' / 'info'
 
 @app.route('/')
 @app.route('/homepage')
@@ -40,6 +38,20 @@ def projects():
     return render_template('projects.html', projects=PROJECTS)
 @app.route('/projects/<project_id>')
 def project_page(project_id):
+    for project in PROJECTS.values():
+        project["desc"] = "No description has been set for this project"
+        try:
+            project["desc"] = (INFO_DIR / f'{project["desctxt"]}.txt').read_text(encoding="utf-8")
+        except FileNotFoundError:
+            app.logger.error("File %s was not found", project["desctxt"])
+
+    for project in PROJECTS.values():
+        project["features"] = "No features have been written for this project"
+        try:
+            project["features"] = (INFO_DIR / f'{project["desctxt"]}features.txt').read_text(encoding="utf-8")
+        except FileNotFoundError:
+            app.logger.error("File for %s was not found", project["title"])
+        
     try:
         project = PROJECTS[project_id]
         return render_template('project.html', project=project)
